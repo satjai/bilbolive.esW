0992c187a804b2d5b9c0b2b56ec2121415db89aa
satjai <satjaisat@gmail.com>
2021-06-01 20:29:22 +0200

    asdf

---
+++ src/firma/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+header('Content-type: text/xml');
+// Incluimos el archivo de clases
+include "class.firmador.php";
+// Declaramos el nuevo objeto Firmador
+$firma = new Firmador();
+$myxml = $firma->firmar('CERT.NOTEBUK.p12','1234','tbaiXml.xml');
+echo $myxml;
+*/
+echo 'bilbolive.esW';
+?>
